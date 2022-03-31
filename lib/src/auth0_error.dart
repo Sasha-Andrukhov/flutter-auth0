062f32802b09e231aadbae3108e66cbede0e2a66
dima <wildmanuse@gmail.com>
2022-03-31 17:32:03 +0300

    updated exception description type

--- lib/src/auth0_error.dart
+++ lib/src/auth0_error.dart
@@ -2,7 +2,7 @@ part of auth0_auth;
 
 class Auth0Exeption implements Exception {
   final String name;
-  final String description;
+  final dynamic description;
   Auth0Exeption(
       {this.name = 'a0.response.invalid', this.description = 'unknown error'});
 
